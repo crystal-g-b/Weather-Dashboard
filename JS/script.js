ec3a9590cc600bd6aa12e779a994d1002dbd530d
crystal-g-b <crystal.barnett1@gmail.com>
2021-03-12 23:21:42 -0500

    started to populate the html file

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+//use open weather api
+
+//var appid
+//get a ref to search form, history list, todat, foreast
+
+//get th ehistory from the local storage and update the search history variabe- if there are no results there should be an empty array
+
+// function to add location to local storage (add to storage)
+ //accept a location as a parameter
+ //add the passed in parameter to existinghostory arrya
+ //update local storage- don't forget to stringfy (JSON)
+
+//function to add an item to searchhistory (add to history)
+ //accept a location as a parameter
+ //create an li
+ //add any classes
+ //set textcontent to walue of parameter
+ //append it to the history list
+
+//loop through searchhistory -- call t to history
+
+//function to get our coordinates (getcoordinates)
+    //accept location as a parameter
